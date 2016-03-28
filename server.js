1eaa558915bde2158c9d208873db911c98f0f370
Martin Carrera <mcarrera.92@gmail.com>
2016-03-28 15:07:12 -0300

    Fixed response

--- server.js
+++ server.js
@@ -5,16 +5,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req, res, next)=> {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-Requested-With,content-type, Authorization, x-access-token');
-    res.setHeader('response_type', 'in_channel')
-    next();
-  });
-
 var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
@@ -31,7 +21,9 @@ router.post('/', function(req, res) {
   console.log('I found a POST');
   console.log(req.body.text);
   console.log('********************--------------------********************');
-  res.json({  message: 'The link' + req.body.text + ' was added successfully'});
+  res.json( { 'response_type': 'in_channel',
+              'text': 'The link ' + req.body.text + ' was added successfully'
+            });
 });
 
 app.use('/api', router);
