c76b1ad7ccfa8fdd01362569ed9387725e7f47b0
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-29 00:35:30 +0200

    Fixed mapper asset helper method

--- helpers/asset-helper.js
+++ helpers/asset-helper.js
@@ -4,27 +4,26 @@ var _ = require('lodash');
 
 module.exports = {
   map: function(asset) {
-    return _.map(asset, function(a) {
-      var slackmessage = a.text;
-      var content = slackmessage.split(' ');
-      return {
-        link   : content.shift(),
-        hashes : content,
-        createdBy: {
-          team: {
-            id: a.team_id,
-            domain: a.team_domain
-          },
-          user: {
-            id: a.user_id,
-            name: a.user_name
-          },
-          channel: {
-            id: a.channel_id,
-            name: a.channel_name
-          }
+    var slackmessage = asset.text;
+    var content = slackmessage.split(' ');
+
+    return {
+      link   : content.shift(),
+      hashes : content,
+      createdBy: {
+        team: {
+          id: asset.team_id,
+          domain: asset.team_domain
+        },
+        user: {
+          id: asset.user_id,
+          name: asset.user_name
+        },
+        channel: {
+          id: asset.channel_id,
+          name: asset.channel_name
         }
-      };
-    });
+      }
+    };
   }
 };
