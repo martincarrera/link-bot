44173c6170a2324f50c69ebfe3a0328eda9b7205
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-28 23:53:38 +0200

    Added asset model and schema

---
+++ models/asset-model.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var assetHelper = require('../helpers/asset-helper');
+
+var Model = require('../libraries/model');
+var Asset = require('../schemas/asset-schema');
+
+var AssetModel = class extends Model {
+  constructor(SchemaModel) {
+    super(SchemaModel);
+  }
+
+  create(input) {
+    var newSchemaModel = new this.SchemaModel(assetHelper.map(input));
+    return newSchemaModel.saveAsync().then(function(doc) {
+      return {
+        'response_type': 'in_channel',
+        text: 'The link ' + doc.link + ' was added successfully by ' + doc.createdBy.user.name
+      };
+    });
+  }
+};
+
+module.exports = new AssetModel(Asset);
---
+++ schemas/asset-schema.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AssetSchema = new Schema({
+
+  link: {
+    type: String,
+    lowercase: true,
+    trim: true
+  },
+
+  hashes: [{
+    type: String,
+    lowercase: true,
+    trim: true
+  }],
+
+  createdBy: {
+    team: {
+      id: String,
+      domain: String,
+    },
+    channel: {
+      id: String,
+      name: String,
+    },
+    user: {
+      id: String,
+      name: String
+    }
+  },
+
+  command: {
+    type: String
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+AssetSchema.pre('save', function(next) {
+  var now = new Date();
+  this.updatedAt = now;
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+module.exports = mongoose.model('Missing', AssetSchema);
