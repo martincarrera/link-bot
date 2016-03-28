e7f8bd4f302dc1d32c186590e91c9843f5ccee4a
Martin Carrera <mcarrera.92@gmail.com>
2016-03-28 13:15:48 -0300

    Added code to log the GET and POSTs received.

---
+++ server.js
@@ -0,0 +1,29 @@
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function(req, res) {
+  console.log('********************--------------------********************');
+  console.log('I found a GET');
+  console.log(req.body);
+  console.log('********************--------------------********************');
+  res.json({ message: 'Welcome to the fucking api' });  
+});
+
+router.post('/', function(req, res) {
+  console.log('********************--------------------********************');
+  console.log('I found a POST');
+  console.log(req.body);
+  console.log('********************--------------------********************');
+  res.json({ message: 'Welcome to the fucking api' });  
+});
+
+app.use('/api', router);
+
+app.listen(port);
