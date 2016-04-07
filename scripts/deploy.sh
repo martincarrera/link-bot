117ac70a32d7f13d11d1167d130c14b6b694c059
gillchristian <gillchristiang@gmail.com>
2016-04-06 21:24:16 -0300

    trying CF deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
-#TARGET_ENV=${1:-"dev"};
+TARGET_ENV=${1:-"dev"};
 
-#CF_SPACE="${CF_SPACE_DEV}";
-#CF_ROUTE=" --random-route";
+CF_SPACE="${CF_SPACE_DEV}";
+CF_ROUTE=" --random-route";
 
-#if [ $TARGET_ENV == "prod" ]; then
-#  CF_SPACE="${CF_SPACE_PROD}";
-#  CF_ROUTE="";
-#fi
+if [ $TARGET_ENV == "prod" ]; then
+  CF_SPACE="${CF_SPACE_PROD}";
+  CF_ROUTE="";
+fi
 
-#echo "Deploying to '$TARGET_ENV' with CF.";
-#echo "Space: $CF_SPACE, Route: $CF_ROUTE."
+echo "Deploying to '$TARGET_ENV' with CF.";
+echo "Space: $CF_SPACE, Route: $CF_ROUTE."
 
-#cf api "${CF_API}";
-#cf auth "${CF_USER}" "${CF_PASSWORD}";
-#cf target -o "${CF_ORG}" -s "${CF_SPACE}";
-#cf push $CF_ROUTE;
-
-
-#########################################################
-
-heroku login
-$HEROKU_USER
-$HEROKU_PASS
-git push heroku master
-heroku ps
+cf api "${CF_API}";
+cf auth "${CF_USER}" "${CF_PASSWORD}";
+cf target -o "${CF_ORG}" -s "${CF_SPACE}";
+cf push $CF_ROUTE;
