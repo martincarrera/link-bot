34e3cf4469cd0479010997b17df70dc9a3431f57
Martin Carrera <mcarrera.92@gmail.com>
2016-04-12 12:28:15 -0300

    Removed pre push tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Run server tests
-RUN_UNIT=true istanbul cover _mocha src/tests/all.js && istanbul check-coverage --statement 50 --line 50 --branch 30 --function 50
-serverStatus=$?
+#RUN_UNIT=true istanbul cover _mocha src/tests/all.js && istanbul check-coverage --statement 50 --line 50 --branch 30 --function 50
+#serverStatus=$?
 
-if [ $serverStatus -ne 0 ]; then
-  echo "Error Server Test fail.";
-fi
-exit $serverStatus;
+#if [ $serverStatus -ne 0 ]; then
+#  echo "Error Server Test fail.";
+#fi
+#exit $serverStatus;
